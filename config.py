b0a5eb13fee419f2cd027f1d6cf47a4e770a5076
Preeti Mitra <preeti.mitra.kol2000@gmail.com>
2026-06-06 22:58:33 +0530

    Add system configurations for anomaly engine

---
+++ config.py
@@ -0,0 +1,4 @@
+# System configurations for the anomaly engine
+WINDOW_SIZE = 10
+ANOMALY_THRESHOLD = 1.8  # Standard deviations away from moving mean
+SENSOR_KEYS = ['voltage', 'frequency', 'temperature']
